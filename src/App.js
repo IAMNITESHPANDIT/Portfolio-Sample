b8093b05e4483f23199ceda2878e65c7dd621665
nitesh <niteshpandit676@gmail.com>
2022-02-19 23:29:30 +0530

    temporary message inserted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import NavBar from './component/NavBar.component';
+import Main from './component/main.component';
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      
+      <Main/>
     </div>
   );
 }
---
+++ src/component/main.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div><>
+    <p className="h2 text-center"> We're working on Project keep touch with us and make sure Fork it ...</p>
+    
+    </></div>
+  )
+}
+
+export default Main
