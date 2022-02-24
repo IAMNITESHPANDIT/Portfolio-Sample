bd71740e9eb3fd0674074c829e86c006ae8d30bb
nitesh <niteshpandit676@gmail.com>
2022-02-24 18:33:04 +0530

    projects added in hire component

--- src/component/NavbarComponent/hire.component.jsx
+++ src/component/NavbarComponent/hire.component.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React from "react";
+import HireImg from "../../assest/hire.svg";
 
 function Hire() {
   return (
-    <div>Hire</div>
-  )
+    <div className="container">
+      <p className="h2 text-center ">Why Should you Hire to Me </p>
+      <hr className="divider" />
+      <div className="container pt-2">
+        <p className="h2 p-2">Projects</p>
+        <div className="row row-cols-1 row-cols-md-2 g-4 pb-4 mb-5">
+          <div className="col">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">
+                  E -Commerce Application with React
+                </h5>
+                <p className="card-text">
+                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
+
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">PortFolio website</h5>
+                <p className="card-text">
+                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
+
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Monster website - A.P.I</h5>
+                <p className="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">COPA I.T.I-Android Application</h5>
+                <p className="card-text">
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
+
+
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">C.S.A C.T.I-Android Application</h5>
+                <p className="card-text">
+                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
+
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">
+                  Open Source Contribution on Github
+                </h5>
+                <p className="card-text">
+                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
+
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hire;
