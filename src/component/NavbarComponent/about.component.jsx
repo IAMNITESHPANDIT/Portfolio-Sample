8bb306e8390b9a0fade72b025c44e6e011dc1726
nitesh <niteshpandit676@gmail.com>
2022-02-24 20:01:19 +0530

    bug fixed

--- src/component/NavbarComponent/about.component.jsx
+++ src/component/NavbarComponent/about.component.jsx
@@ -1,91 +1,83 @@
 import React from 'react'
 import Typical from  'react-typical';
+import aboutImg from "../../assest/about us.svg";
+
 
 function About() {
   return (
     <div className="container">
-       <p className="h1 text-center mt-4 ">About Us</p>
+      <div className="m-4 p-4">
+       <p className="h1 text-center">About Us</p>
        <hr class="dropdown-divider"/>
-      <div className="shadow-lg p-3 mb-5 bg-body mt-5 ms-5 p-4 rounded">
-       <div className="row">
-         <div className="col-6">
-         <p className="h2">Name :-</p>
-         <p className="h2">Skills :-</p>
-         <p className="h2">Github :-</p>
-         <p className="h2">Linkdin :-</p>
-         <p className="h2">Insta :-</p>
-         </div>
-         <div className="col-6">
-          
-         <p className="h2">
-         <Typical className="nameText h2" loop={Infinity}
-              steps={[
-                "NITESH KUMAR PANDEY",
-                1000,
-                "KUMAR",
-                1000,
-                "PANDEY",
-                1000,
-      
-              ]}
-              />
-         </p>
-         <p className="h2">
-         <span>
-         <Typical className="myText h2" loop={Infinity}
-              steps={[
-                "Mysql",
-                1000,
-                "Reactjs",
-                1000,
-                "Saas",
-                1000,
-                "C++",
-                1000,
-                "Python",
-                1000,
-                "Android Development",
-                1000,
-                "Data Structures",
-                1000,
-                "C",
-                1000,
-                "HTML5",
-                1000,
-                "Mongo Db",
-                1000,
-                "Git Bash",
-                1000,
-                "Problem Solving",
-                1000,
-                "JavaScript",
-                1000,
-                "PHP",
-                1000,
-                "Bootstrap",
-                1000,
-                
-              ]}
-              />
-         </span>
-         </p>
-         <br/>
+       <div className="row p-5">
+          <div className="shadow-lg col-12 col-md-5 bg-body m-1 rounded">
+              <div className="row">
+                <div className="col-6 text-start">
+                    <p className="h2">Skills :-</p>
+                   <br/>
+                    <p className="h2">Github :-</p>
+                   <br/>
+                    <p className="h2">Linkdin :-</p>
+                   <br/>
+                    <p className="h2">Insta :-</p>
+                  </div>
+         <div className="col-6 "> 
+             <p className="h2">
+                <span>
+                  <Typical className="myText h2 text-start" loop={Infinity}
+                       steps={[
+                         "Mysql",
+                         1000,
+                         "Reactjs",
+                         1000,
+                         "Saas",
+                         1000,
+                         "C++",
+                         1000,
+                         "Python",
+                         1000,
+                         "Android Dev",
+                         1000,
+                         "Data Structures",
+                         1000,
+                         "C",
+                         1000,
+                         "HTML5",
+                         1000,
+                         "Mongo Db",
+                         1000,
+                         "Git Bash",
+                         1000,
+                         "JavaScript",
+                         1000,
+                         "PHP",
+                         1000,
+                         "Bootstrap",
+                         1000,
 
-         <a href="https://github.com/IAMNITESHPANDIT">Github</a>
-         <br/>
-         <br/>
-         <a href="https://www.linkedin.com/in/iamniteshpandit/">Linkdin</a>
-         <br/>
-         <br/>
-         <a href="https://www.instagram.com/iamniteshpandit/">Instagram</a>
-         </div>
+                       ]}
+                       />
+                </span>
+                 </p>
+                   <br/>
+                    <a href="https://github.com/IAMNITESHPANDIT">Github</a>
+                    <br/>
+                    <br/>
+                    <a href="https://www.linkedin.com/in/iamniteshpandit/">Linkdin</a>
+                    <br/>
+                    <br/>
+                    <a href="https://www.instagram.com/iamniteshpandit/">Instagram</a>
+                    </div>
        </div>
 
 
+          </div>
+          <div className="col-12 col-md-6 m-1 shadow-lg bg-body rounded ">
+            <img src={aboutImg} alt="about image" className="img-fluid p-3" />
+          </div>
       </div>
 
-
-
+      </div>                 
     </div>
   )
 }
--- src/component/main.component.jsx
+++ src/component/main.component.jsx
@@ -39,7 +39,7 @@ function Main() {
             </div>
 
         </div>
-        <div className="col-12 col-md-6 p-4 mb-5">
+        <div className="col-12 col-md-6 p-4 mb-5 d-none d-md-block">
          <img className="img-fluid" alt="programming logo" src={Diamond}/>
         </div>
       </div>
