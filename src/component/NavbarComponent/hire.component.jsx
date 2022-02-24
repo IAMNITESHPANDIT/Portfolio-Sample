480cffebdda2d982ee32d974c2622d91259c933c
nitesh <niteshpandit676@gmail.com>
2022-02-24 19:30:09 +0530

    hire Component Successfully Design

--- src/component/NavbarComponent/hire.component.jsx
+++ src/component/NavbarComponent/hire.component.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import HireImg from "../../assest/hire.svg";
+import androidImg from "../../assest/images/android.jpg";
+import ecomImg from "../../assest/images/ecom.jpg";
+import monsterImg from "../../assest/images/monster.jpg";
+import ossImg from "../../assest/images/oss.jpg";
+import portImg from "../../assest/images/port.jpg";
+
 
 function Hire() {
   return (
@@ -8,16 +13,16 @@ function Hire() {
       <hr className="divider" />
       <div className="container pt-2">
         <p className="h2 p-2">Projects</p>
-        <div className="row row-cols-1 row-cols-md-2 g-4 pb-4 mb-5">
+        <div className="row row-cols-1 row-cols-md-2 g-4 pb-4 mb-5 myImg">
           <div className="col">
             <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
+              <img src={ecomImg} className="card-img-top img-fluid" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">
+                <h5 className="card-title h2 ">
                   E -Commerce Application with React
                 </h5>
                 <p className="card-text">
-                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
 
                 </p>
               </div>
@@ -25,9 +30,9 @@ function Hire() {
           </div>
           <div className="col">
             <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
+              <img src={portImg} className="card-img-top img-fluid" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">PortFolio website</h5>
+                <h5 className="card-title h2 ">PortFolio website</h5>
                 <p className="card-text">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
 
@@ -37,47 +42,46 @@ function Hire() {
           </div>
           <div className="col">
             <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
+              <img src={monsterImg} className="card-img-top img-fluid" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Monster website - A.P.I</h5>
+                <h5 className="card-title h2 ">Monster website - A.P.I</h5>
                 <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
+
                 </p>
               </div>
             </div>
           </div>
           <div className="col">
             <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
+              <img src={androidImg} className="card-img-top img-fluid" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">COPA I.T.I-Android Application</h5>
+                <h5 className="card-title h2 ">COPA I.T.I-Android Application</h5>
+                <p className="card-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
+
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="card">
+              <img src={androidImg} className="card-img-top img-fluid" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title h2 ">C.S.A C.T.I-Android Application</h5>
                 <p className="card-text">
                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
 
-
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col">
-            <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">C.S.A C.T.I-Android Application</h5>
-                <p className="card-text">
-                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aut accusantium animi sit. Voluptatum repellendus eligendi porro, perspiciatis iure error.
-
                 </p>
               </div>
             </div>
           </div>
           <div className="col">
             <div className="card">
-              <img src="..." className="card-img-top" alt="..." />
+              <img src={ossImg} className="card-img-top img-fluid" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">
+                <h5 className="card-title h2 ">
                   Open Source Contribution on Github
                 </h5>
                 <p className="card-text">
